7a52ed52056c79da9eb0071bfa892f52bddb08c2
FBlachucik <146762475+FBlachucik@users.noreply.github.com>
2024-09-25 13:12:06 +0200

    Add files via upload

---
+++ kantor1/app/calc.php
@@ -0,0 +1,70 @@
+<?php
+// KONTROLER strony kantora
+require_once dirname(__FILE__).'/../config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+// 1. pobranie parametrów
+
+$kwota = $_REQUEST ['kwota'];
+$kurs = $_REQUEST ['kurs'];
+$math = $_REQUEST ['prz'];
+
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+// sprawdzenie, czy parametry zostały przekazane
+if ( ! (isset($kwota) && isset($kurs) && isset($math))) {
+	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+// sprawdzenie, czy potrzebne wartości zostały przekazane
+if ( $kwota == "") {
+	$messages [] = 'Nie podano liczby 1';
+}
+if ( $kurs == "") {
+	$messages [] = 'Nie podano liczby 2';
+}
+
+//nie ma sensu walidować dalej gdy brak parametrów
+if (empty( $messages )) {
+	
+	// sprawdzenie, czy $x i $y są liczbami całkowitymi
+	if (! is_numeric( $kwota )) {
+		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $kurs )) {
+		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
+	}	
+
+}
+
+// 3. wykonaj zadanie jeśli wszystko w porządku
+
+if (empty ( $messages )) { // gdy brak błędów
+	
+	$kwota = floatval($kwota);
+	$kurs = floatval($kurs);
+
+	//konwersja wartości z radio buttonów na $_POST
+
+	$przelicznik = $_POST['prz'];
+
+	//wykonanie operacji
+	
+	if ($przelicznik == "euroZlote") {          
+		$result = $kwota * $kurs;     
+	}
+	else {
+		$result = ($kwota / $kurs);
+		$result = round($result,2);
+	}          
+}
+
+// 4. Wywołanie widoku z przekazaniem zmiennych
+// - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
+//   będą dostępne w dołączonym skrypcie
+include 'calc_view.php';
