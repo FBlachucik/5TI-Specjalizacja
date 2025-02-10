da0c1a800561476df47672a123703b3b8ae1495e
FBlachucik <146762475+FBlachucik@users.noreply.github.com>
2025-02-10 12:58:22 +0100

    kantor4

---
+++ kantor4/app/templates_c/8acaa13a6eb21a249efea5f035519049a0440010_0.file_main.html.php
@@ -0,0 +1,94 @@
+<?php
+/* Smarty version 5.4.1, created on 2025-01-15 11:59:17
+  from 'file:C:\xampp\htdocs\php_04_uproszczony\app\../templates/main.html' */
+
+/* @var \Smarty\Template $_smarty_tpl */
+if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
+  'version' => '5.4.1',
+  'unifunc' => 'content_678795058019e5_37390438',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '8acaa13a6eb21a249efea5f035519049a0440010' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\php_04_uproszczony\\app\\../templates/main.html',
+      1 => 1490288994,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+))) {
+function content_678795058019e5_37390438 (\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\php_04_uproszczony\\templates';
+$_smarty_tpl->getInheritance()->init($_smarty_tpl, false);
+?>
+<!doctype html>
+<html lang="pl">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php echo (($tmp = $_smarty_tpl->getValue('page_description') ?? null)===null||$tmp==='' ? 'Opis domyślny' ?? null : $tmp);?>
+">
+	<title><?php echo (($tmp = $_smarty_tpl->getValue('page_title') ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</title>
+	<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-UQiGfs9ICog+LwheBSRCt1o5cbyKIHbwjWscjemyBMT9YCUMZffs6UqUTd0hObXD" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?php echo $_smarty_tpl->getValue('app_url');?>
+/css/style.css">	
+</head>
+<body>
+
+<div class="header">
+	<h1><?php echo (($tmp = $_smarty_tpl->getValue('page_title') ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</h1>
+	<h2><?php echo (($tmp = $_smarty_tpl->getValue('page_header') ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</h1>
+	<p>
+		<?php echo (($tmp = $_smarty_tpl->getValue('page_description') ?? null)===null||$tmp==='' ? "Opis domyślny" ?? null : $tmp);?>
+
+	</p>
+</div>
+
+<div class="content">
+<?php 
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_1418921426678795057ffdf4_52906716', 'content');
+?>
+
+</div><!-- content -->
+
+<div class="footer">
+	<p>
+<?php 
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_189615470767879505800f83_64991929', 'footer');
+?>
+
+	</p>
+	<p>
+		Widok oparty na stylach <a href="http://purecss.io/" target="_blank">Pure CSS Yahoo!</a>. (autor przykładu: Przemysław Kudłacik)
+	</p>
+</div>
+
+</body>
+</html><?php }
+/* {block 'content'} */
+class Block_1418921426678795057ffdf4_52906716 extends \Smarty\Runtime\Block
+{
+public function callBlock(\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\php_04_uproszczony\\templates';
+?>
+ Domyślna treść zawartości .... <?php
+}
+}
+/* {/block 'content'} */
+/* {block 'footer'} */
+class Block_189615470767879505800f83_64991929 extends \Smarty\Runtime\Block
+{
+public function callBlock(\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\php_04_uproszczony\\templates';
+?>
+ Domyślna treść stopki .... <?php
+}
+}
+/* {/block 'footer'} */
+}
